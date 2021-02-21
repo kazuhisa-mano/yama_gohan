85d211961e0c63c9c52f4778201fc747de1853f4
kazuhisa-mano <route_tri_314@yahoo.co.jp>
2021-02-21 13:56:33 +0000

    add post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
+  def index
+  end  
   def new
+    @post = Post.new
+    @tags = Tag.all
   end
   
   def create
-  end  
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    tag_list = params[:post][:tag_name].split(nil) #viewからタグの名前を一緒に受け取る
+    if @post.save
+      @post.save_tag(tag_list) #save_tagはapp/models/post.rbに定義
+      redirect_to posts_path
+    else
+      render 'new'
+    end  
+  end
+  
+  private
+    def post_params
+      params.require(:post).permit(:name, :image, :material, :cooking_time)
+    end  
 end
