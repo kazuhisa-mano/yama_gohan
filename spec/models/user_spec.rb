4e0d1f4df59385282930974ffea89195a1471fd7
kazuhisa-mano <route_tri_314@yahoo.co.jp>
2021-03-15 21:51:48 +0000

    [add] ログインメッセージ

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "データが正しく保存される" do
-    before do
-      @user = User.new
-      @user.name = "test-name"
-      @user.email = "test@test"
-      @user.password = "aaaaaa"
-      @user.save
-    end
-    it "全て入力されてあるので保存される" do
-      expect(@user).to be_valid
-    end
-  end
+  # context "データが正しく保存される" do
+  #   before do
+  #     @user = User.new
+  #     @user.name = "test-name"
+  #     @user.email = "test@test"
+  #     @user.password = "aaaaaa"
+  #     @user.save
+  #   end
+  #   it "全て入力されてあるので保存される" do
+  #     expect(@user).to be_valid
+  #   end
+  # end
+  
+  it "is valid with a name, email and password" do
+    user = User.new(
+      name: "taro",
+      email: "rspec@test",
+      password: "aaaaaa"
+    )
+    expect(user).to be_vaild
+  end 
 end
