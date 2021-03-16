1fd5a6ef379038b155d003fef1a37fd2f44158b2
kazuhisa-mano <route_tri_314@yahoo.co.jp>
2021-03-16 21:52:49 +0000

    [add] モデルテスト;アソシエーション

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+  
+  describe 'バリデーションのテスト' 
 end
