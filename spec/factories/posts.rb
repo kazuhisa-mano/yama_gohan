b7679db23bfc025ffb298ee7ff84ccdca17a6811
kazuhisa-mano <route_tri_314@yahoo.co.jp>
2021-03-28 06:59:59 +0000

    [add] RSpec Postモデル

---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :post do
+    name         {"ラーメン"}
+    image_id        {"1"}
+    material     {"インスタントラーメン"}
+    recipe       {"お湯でゆでる"}
+    cooking_time {"5"}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,53 +43,5 @@ RSpec.describe User, type: :model do
       user = User.new(introduction: "a" * 101)
       expect(user.invalid?).to be true
     end
-
-  #   it "name, email, password, password_confirmationが存在すれば登録できること" do
-  #     user = User.new(
-  #       name: "test",
-  #       email: "rspec@test",
-  #       password: "123456",
-  #       password_confirmation: "123456"
-  #       )
-  #       expect(user).to be_valid
-  #   end
-
-  #   it "passwordがない場合は登録できないこと" do
-  #     user = User.new(
-  #       id: "1",
-  #       name: "test",
-  #       email: "rspec@test",
-  #       password: "",
-  #       introduction: "sample",
-  #       profile_image_id: "1",
-  #       created_at: "2021-02-28 07:03:14",
-  #       updated_at: "2021-02-28 07:03:14"
-  #       )
-  #       user.valid?
-  #       expect(user.errors[:password]).to include("can't be blank")
-  #   end
-
-  #   it "password_confirmationがないと登録できないこと" do
-  #     user = User.new(
-  #       name: "test",
-  #       email: "rspec@test",
-  #       password: "123456",
-  #       password_confirmation: "",
-  #       )
-  #       user.valid?
-  #       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-  #   end
-
-  #   it "emailがない場合は登録できないこと" do
-  #     user = User.new(
-  #       name: "test",
-  #       email: "",
-  #       password: "123456",
-  #       password_confirmation: "123456",
-  #       )
-  #       user.valid?
-  #       expect(user.errors[:email]).to include("can't be blank")
-  #   end
-  # end
   end
 end
