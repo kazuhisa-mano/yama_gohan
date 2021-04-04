f33eca40e882982b7578bf16c058cc3b133f2e5a
kazuhisa-mano <route_tri_314@yahoo.co.jp>
2021-04-04 21:43:49 +0000

    [add] RSpec posts_request

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,33 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  # describe '編集のテスト' do
-  #   context '自分の投稿の編集画面への遷移' do
-  #     it '遷移できる' do
-  #       visit edit_post_path(post)
-  #       expect(current_path).to eq('/posts/' + post.id.to_s + '/edit')
-  #     end
-  #   end
-  # end  
+  describe "#index" do
+    context "投稿一覧ページが正しく表示される" do
+      before do
+        get posts_path
+      end
+      it "リクエストは200 OKとなること" do
+        expect(response.status).to eq 200
+      end
+      it "タイトルが正しく表示されていること" do
+        expect(response.body).to include("投稿一覧")
+      end
+    end
+  end
   
-  # describe '新規投稿ページ' do
-  #   context "新規投稿ページが正しく表示される" do
-  #     let(:user) { create(:user) }
-  #     let!(:user2) { create(:user) }
-  #     let!(:book) { create(:book, user: user) }
-  #     let!(:book2) { create(:book, user: user2) }
-  #     before do
-  #       visit new_user_session_path
-  # 	    fill_in 'user[name]', with: user.name
-  # 	    fill_in 'user[password]', with: user.password
-  # 	    click_button 'ログイン'
-  #     end
-  #     before do
-  #       get new_post_path
-  #     end
-  #     it 'リクエストは200 OKとなること' do
-  #       expect(response.status).to eq 200
-  #     end
-  #   end
-  # end
+  
 end
